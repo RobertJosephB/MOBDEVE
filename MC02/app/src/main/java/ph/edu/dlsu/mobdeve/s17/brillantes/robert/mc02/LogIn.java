1b48b080ebcfd8405c6b13ed7f4a1b7c42a27721
RobertJosephB <77220807+RobertJosephB@users.noreply.github.com>
2021-08-19 13:36:46 +0800

    Add files via upload
    Forgot to change package name

---
+++ MC02/app/src/main/java/ph/edu/dlsu/mobdeve/s17/brillantes/robert/mc02/LogIn.java
@@ -0,0 +1,14 @@
+package ph.edu.dlsu.mobdeve.s17.brillantes.robert.mc02;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LogIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in);
+    }
+}
