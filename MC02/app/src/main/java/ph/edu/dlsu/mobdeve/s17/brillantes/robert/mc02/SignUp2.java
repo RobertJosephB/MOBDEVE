c7ca11d64971bf60b36b3ae507a4ecbff839cfce
RobertJosephB <77220807+RobertJosephB@users.noreply.github.com>
2021-09-03 13:58:41 +0800

    Add files via upload

--- MC02/app/src/main/java/ph/edu/dlsu/mobdeve/s17/brillantes/robert/mc02/SignUp2.java
+++ MC02/app/src/main/java/ph/edu/dlsu/mobdeve/s17/brillantes/robert/mc02/SignUp2.java
@@ -2,23 +2,41 @@ package ph.edu.dlsu.mobdeve.s17.brillantes.robert.mc02;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.TextView;
 
 import com.google.android.material.snackbar.Snackbar;
+
+import java.util.Calendar;
 
 import ph.edu.dlsu.mobdeve.s17.brillantes.robert.mc02.databinding.ActivitySignUp2Binding;
 
 public class SignUp2 extends AppCompatActivity {
 
     private ActivitySignUp2Binding binding;
+    private TextView displayDate;
+    private DatePickerDialog.OnDateSetListener dateSetListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivitySignUp2Binding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        displayDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar cal = Calendar.getInstance();
+                int year = cal.get(Calendar.YEAR);
+                int month = cal.get(Calendar.MONTH);
+                int day = cal.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog = new DatePickerDialog(SignUp2.this);
+            }
+        });
 
         Bundle bundle = getIntent().getExtras();
 
